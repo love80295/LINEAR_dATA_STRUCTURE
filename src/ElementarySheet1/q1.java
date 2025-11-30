b79b516b183235c8908f2893f841a6b9100f928e
loveagrawal <loveagrawal80295@gmail.com>
2025-11-30 21:28:56 +0530

    1

---
+++ src/ElementarySheet1/q1.java
@@ -0,0 +1,14 @@
+package ElementarySheet1;
+import java.util.*;
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println(2);
+        }else{
+            System.out.println(1);
+        }
+
+    }
+}
