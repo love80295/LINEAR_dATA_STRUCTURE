47a2389c0a0748327da555d2b96df3ec77d29e0c
loveagrawal <loveagrawal80295@gmail.com>
2025-12-04 06:27:12 +0530

    1

---
+++ src/HASHSET/UniqueEmails.java
@@ -0,0 +1,10 @@
+package HASHSET;
+import java.util.*;
+public class UniqueEmails {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[]arr = str.toLowerCase().split(" ");
+        // same as unique words
+    }
+}
