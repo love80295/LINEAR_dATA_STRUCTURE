f0426c1afb61ebea8f931a3057ab42dab42c0896
loveagrawal <loveagrawal80295@gmail.com>
2026-01-04 16:44:47 +0530

    1

---
+++ src/LINKED_LIST/Questions/DeleteNode.java
@@ -0,0 +1,20 @@
+package LINKED_LIST.Questions;
+
+public class DeleteNode {
+    public static void main(String[] args) {
+        /**
+         * Definition for singly-linked list.
+         * public class ListNode {
+         *     int val;
+         *     ListNode next;
+         *     ListNode(int x) { val = x; }
+         * }
+         */
+
+    }
+}
+class Solution {
+    public void deleteNode(DeleteNode node) {
+
+    }
+}
