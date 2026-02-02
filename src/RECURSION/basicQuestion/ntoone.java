2bb2a072f9ece12f9d82bf3ef819109fd2008531
loveagrawal <loveagrawal80295@gmail.com>
2026-02-02 15:18:14 +0530

    1

---
+++ src/RECURSION/basicQuestion/ntoone.java
@@ -0,0 +1,12 @@
+package RECURSION.basicQuestion;
+
+public class ntoone {
+    public static void main(String[] args) {
+        number(5);
+    }
+    static void number(int n){
+        if(n<0) return  ;
+        number(n-1);
+        System.out.println(n);
+    }
+}
